1f2957db7010be89d6448baddc1d628d56c4be17
Aswin Haridas <aswinvharidas@gmail.com>
2020-08-09 13:40:34 +0530

    images added, along with custom dialog and fullscreen capability

---
+++ app/src/main/java/com/greenbox/galleryapp/FullViewActivity.java
@@ -0,0 +1,13 @@
+package com.greenbox.galleryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_view);
+    }
+}
